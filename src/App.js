04b0ffdc4baf6137b992080a1a8d62d01149ba24
kongmenglor <lorxx504@umn.edu>
2023-06-16 14:01:44 -0500

    commenting out components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 function App() {
   const name = <h1>Kongmeng Lor</h1>
+  // return <div className='App'>
+  //     <Job salary={90000} position="Senior SDE" company="Amazon" />
+  //     <Job salary={12000} position="Junior SDW" company="Google" />
+  //     <Job salary={10000} position="Project Manager" company="Netflix"/>
+  // </div>
+
   return <div className='App'>
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={12000} position="Junior SDW" company="Google" />
-      <Job salary={10000} position="Project Manager" company="Netflix"/>
+  
   </div>
 }
 
@@ -20,12 +24,12 @@ function App() {
 // }
 
 
-const Job = (data) => {
-  return <div>
-    <h1>{data.salary}</h1>
-    <h1>{data.position}</h1>
-    <h1>{data.company}</h1>
-  </div>
-}
+// const Job = (data) => {
+//   return <div>
+//     <h1>{data.salary}</h1>
+//     <h1>{data.position}</h1>
+//     <h1>{data.company}</h1>
+//   </div>
+// }
 export default App;
  
